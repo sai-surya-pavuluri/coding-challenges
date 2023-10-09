15479ad7f2154f9d7c0fc00549a1c168f896d5aa
Sai Surya Pavuluri <105671468+sai-surya-pavuluri@users.noreply.github.com>
2023-10-09 15:06:22 +0000

    Binary Gap Task Commit

---
+++ Binary Gap/Solution.java
@@ -0,0 +1,18 @@
+
+import java.util.regex.*;
+
+class Solution {
+    public int solution(int N) {
+        String binary = Integer.toBinaryString(N);
+        int optimalGap = 0;
+        Pattern pattern = Pattern.compile("10*1");
+        Matcher matcher = pattern.matcher(binary);
+        while(matcher.find()){
+            int foundGap = (matcher.group().length() - 2);
+            if(optimalGap < foundGap){
+                optimalGap = foundGap;
+            }
+        }
+        return optimalGap;
+    }
+}
